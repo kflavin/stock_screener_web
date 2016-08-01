53f4f1e04654698dfe1229f84ad837c528795c86
Kyle <Kyle.Flavin@gmail.com>
2016-08-01 00:10:40 -0700

    Generate fake indicators.

--- app/models.py
+++ app/models.py
@@ -29,6 +29,9 @@ class User(db.Model, UserMixin):
     current_login_ip = db.Column(db.String(255))
     login_count = db.Column(db.Integer)
 
+    def __repr__(self):
+        return "<Userid: {0}, Email: {1}>".format(self.id, self.email)
+
 class Company(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
@@ -69,3 +72,27 @@ class Indicators(db.Model):
     date = db.Column(db.Date)
     roe = db.Column(db.Float)
     company_id = db.Column(db.Integer, db.ForeignKey('company.id'))
+
+    @staticmethod
+    def generate_fake(count=100):
+        import forgery_py
+        from random import random, seed
+        from sqlalchemy.exc import IntegrityError
+        from datetime import datetime
+
+        seed()
+        companies = Company.query.all()
+        for company in companies:
+            i = Indicators(date=datetime.now(),
+                           roe = "{0:.2f}".format(random()*0.5),
+                           company_id = company.id
+                           )
+            db.session.add(i)
+            try:
+                db.session.commit()
+            except IntegrityError:
+                db.session.rollback()
+
+    def __repr__(self):
+        return "<Symbol: {symbol}, Date: {date}>".format(symbol=self.company.symbol, date=self.date)
+
