b5cb24b85befe48c0a604ad7651da43f3eefa55d
Kyle <Kyle.Flavin@gmail.com>
2016-09-17 00:19:28 -0700

    Additional files for API, and updated error messages.

--- app/main/views.py
+++ app/main/views.py
@@ -274,16 +274,6 @@ def listings(page):
                            )
 
 
-@main.app_errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@main.app_errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
-
 @main.route('/logout')
 @login_required
 def logout():
