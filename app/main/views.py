a5510ecb1ca8de2e0919bf78ec029a88dcff385a
Kyle <Kyle.Flavin@gmail.com>
2017-01-07 13:25:20 -0800

    Consolidate industry and sector averages

--- app/main/views.py
+++ app/main/views.py
@@ -10,7 +10,6 @@ from sqlalchemy import create_engine, desc, asc, func
 from sqlalchemy.sql.expression import nullslast
 from app.main.pages import Pagination
 from app.main.forms import FilterForm
-from app.utils import get_averages
 from . import main
 from .. import db
 from ..models import Indicators, Company
@@ -175,6 +174,8 @@ def company_detail(symbol):
 
     sector_averages = get_averages("sector", company.sector)
     industry_averages = get_averages("industry", company.industry)
+    print "sector", sector_averages
+    print "industry", industry_averages
 
     # # entities to display for sector averages
     # entities = get_entities(with_symbol=False)
@@ -399,3 +400,56 @@ def logout():
 def send_css(path):
     print "Your path is", path
     return send_from_directory('static/css', path)
+
+
+def get_averages(type, search):
+    """
+    Return a dictionary with averages for each indicator in the given sector
+    Args:
+        type: sector or industry
+        sector: search string
+
+    Returns: dict of averages with indicator as key
+
+    """
+    # entities to display for averages
+    entities = get_entities(with_symbol=False)
+    wrapped_entities = []
+    for entity in entities:
+        wrapped_entities.append(func.avg(entity).label(entity.key))
+
+    averages_d = {}
+    if search is not None:
+        stmt1 = db.session.query(Indicators.company_id, func.max(Indicators.id).label('max_id')).group_by(
+            Indicators.company_id
+        ).subquery()
+        stmt2 = db.session.query(Company, stmt1.c.max_id).join(stmt1, stmt1.c.company_id == Company.id).filter(
+            setattr(Company, type, search)
+        ).subquery()
+        averages = db.session.query(*wrapped_entities). \
+            join(stmt2, stmt2.c.max_id == Indicators.id). \
+            filter((Indicators.roe != None) & (Indicators.fcf != None) & (Indicators.ev2ebitda != None)). \
+            first()
+
+        for e in entities:
+            averages_d[e.key] = getattr(averages, e.key)
+
+    return averages_d
+
+
+def get_entities(with_symbol=True, with_date=False):
+    order_bys = Indicators.get_attributes(with_symbol=with_symbol)
+
+    # configure models (for determining column model) and entities (for retrieving columns)
+    entities = []
+    models = []
+    for o in order_bys:
+        if o.find(".") != -1:
+            entities.append(eval(o))
+            models.append(o.split(".")[0])
+        else:
+            entities.append(eval("Indicators." + o))
+    if with_date:
+        entities.append(Indicators.date)
+
+    return entities
