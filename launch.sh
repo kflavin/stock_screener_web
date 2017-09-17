d68004938cca6d12744c99767f55e02c5dbfa047
Kyle <Kyle.Flavin@gmail.com>
2017-09-17 16:15:35 -0700

    updates

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #echo "hello world" >> /app/myfile
 #echo `pwd` >> /app/myfile
-#gunicorn manage:app
-
+( gunicorn manage:app & )
+ps -ef | grep gunicorn
 cd stocks-cli/dist; python -m SimpleHTTPServer $PORT
